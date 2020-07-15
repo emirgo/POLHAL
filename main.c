8bbf286457d363fa80c06d18fa3c5f51de7c974b
Emir <40762681+emirgo@users.noreply.github.com>
2020-07-15 16:07:02 +0200

    Blinking the gpio

--- main.c
+++ main.c
@@ -2,17 +2,21 @@
 
 int main(void)
 {
+	// Initialize the clock
 	// IOPCEN
-	RCC->APB2ENR |= 0x10;
+	RCC->APB2ENR = 1 << 4;
 	// Set PC13 as output
-	GPIOC->CRH |= 0x300000;
+	// OUTPUT @ 50Mhz
+	GPIOC->CRH |= ((1 << 20) | (1 << 21));
+	// GPO
+	GPIOC->CRH &= ~((1 << 22) | (1 << 23));
 	
 	while(1)
 	{
-		GPIOC->BSRR = 1 << 13;
-		for(int i = 0; i < 0xFFFFF; i++);
-		GPIOC->BSRR = 1 << 29;
-		for(int i = 0; i < 0xFFFFF; i++);
+		GPIOC->BSRR = (1 << 13);
+		for(int i = 0; i <= 50000000; i++);
+		GPIOC->BSRR = (1 << (13 + 16));
+		for(int i = 0; i <= 50000000; i++);
 	}
 	
 	return 0;
