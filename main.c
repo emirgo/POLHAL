0b490baa28c5157a2b7cd8df56456040895c96bf
Emir <40762681+emirgo@users.noreply.github.com>
2020-08-06 20:18:32 +0200

    test blink

--- main.c
+++ main.c
@@ -2,13 +2,36 @@
 
 #include "_POLHAL.h"
 
+void testBlink();
+
 int main(void)
 {
 	
 	while(1)
 	{
-		
+		testBlink();
 	}
 	
 	return 0;
+}
+
+/* Register examples */
+
+// On board blink
+void testBlink()
+{
+	// Enable clock for GPIOA
+	RCC->AHB1ENR |= 1;
+	// GPIO mode for PA5
+	GPIOA->MODER |= 1 << 10;
+	// Output Speed
+	GPIOA->OSPEEDR |= 1 << 10 | 1 << 11;
+	
+	while (1)
+	{
+		GPIOA->ODR ^= 1 << 5;
+		volatile int i = 0;
+		for (i = 0; i < 0xfffff; i++);
+	}
+	
 }
