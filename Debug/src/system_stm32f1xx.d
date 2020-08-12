75d7681b67baee2b9113419f43bce3dcd94cf5cc
emirgo <emirg@protonmail.ch>
2020-08-12 16:18:30 +0200

    cpp, eclipse and PINPOS failing

---
+++ Debug/src/system_stm32f1xx.d
@@ -0,0 +1,22 @@
+src/system_stm32f1xx.o: ../src/system_stm32f1xx.c \
+ /home/heurisitk/dev/POLHAL/CMSIS/device/stm32f1xx.h \
+ /home/heurisitk/dev/POLHAL/CMSIS/device/stm32f103xb.h \
+ /home/heurisitk/dev/POLHAL/CMSIS/core/core_cm3.h \
+ /home/heurisitk/dev/POLHAL/CMSIS/core/core_cmInstr.h \
+ /home/heurisitk/dev/POLHAL/CMSIS/core/cmsis_gcc.h \
+ /home/heurisitk/dev/POLHAL/CMSIS/core/core_cmFunc.h \
+ /home/heurisitk/dev/POLHAL/CMSIS/device/system_stm32f1xx.h
+
+/home/heurisitk/dev/POLHAL/CMSIS/device/stm32f1xx.h:
+
+/home/heurisitk/dev/POLHAL/CMSIS/device/stm32f103xb.h:
+
+/home/heurisitk/dev/POLHAL/CMSIS/core/core_cm3.h:
+
+/home/heurisitk/dev/POLHAL/CMSIS/core/core_cmInstr.h:
+
+/home/heurisitk/dev/POLHAL/CMSIS/core/cmsis_gcc.h:
+
+/home/heurisitk/dev/POLHAL/CMSIS/core/core_cmFunc.h:
+
+/home/heurisitk/dev/POLHAL/CMSIS/device/system_stm32f1xx.h:
